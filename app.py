a3f02ce62e94a4833b99deb55b786fe0309fb11e
SiemensHalske <hendrksiemens1@gmail.com>
2023-12-13 13:11:39 +0100

    Add JWT cookie CSRF protection and disable access token expiration

--- app.py
+++ app.py
@@ -60,6 +60,8 @@ app.config['JWT_SECRET_KEY'] = generate_secret_key()
 
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config['JWT_COOKIE_SECURE'] = True
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
 
 jwt = JWTManager(app)
 

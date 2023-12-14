1cc4584dcd2c4adf8ef15f7dfe26b1ef698b4b74
Florian Ostendorf <florian.ostendorf@d-velop.de>
2023-12-14 11:31:24 +0100

    making extension path relative, ignoring all .pyc files

--- extensions.py
+++ extensions.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-database_path = 'C:\\Users\\Hendrik\\Documents\\Github\\Trainex aber besser\\database\\users.db'
+database_path = '.\\database\\users.db'
 
 db = SQLAlchemy()
 login_manager = LoginManager()

7bbec1544122bfdfc38a52731f42e7314caabd63
SiemensHalske <hendrksiemens1@gmail.com>
2023-12-22 01:30:18 +0100

    Add debug prints to logout function

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -108,6 +108,8 @@ def login() -> str:
 def logout() -> str:
     print("Logout")  # Debug print
     print(f"JWT: {get_jwt_identity()} with role {get_jwt_identity()['role']}")  # Debug print
+    print(f"JWT: {get_jwt_identity()} with username {get_jwt_identity()['username']}")  # Debug print
+    print("--------------------")  # Debug print
     response = make_response(redirect(url_for('auth.login')))
     unset_jwt_cookies(response, 'access_token_cookie')
     logout_user()  # Flask-Login's logout_user Funktion aufrufen, falls verwendet

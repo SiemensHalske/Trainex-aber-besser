e94cb127b74ddf288cf4c7e3242ced9e05d81055
SiemensHalske <hendrksiemens1@gmail.com>
2023-12-23 22:21:40 +0100

    Add error handling for database connection and event insertion
    This commit adds error handling to the `create_connection` and `add_event` functions in `event_importer.py`. If there is an error connecting to the database or inserting an event, an appropriate error message is printed and the program exits with the corresponding exit code. Additionally, error handling is added to the `bulk_add_events` function to handle file not found errors. This improves the robustness of the program and provides better feedback to the user.

--- testarea/event_importer.py
+++ testarea/event_importer.py
@@ -9,6 +9,12 @@ DB_CONFIG = {
     'host': 'localhost'
 }
 
+# Exit codes
+EXIT_SUCCESS = 0
+EXIT_INVALID_ARGUMENTS = 1
+EXIT_FILE_NOT_FOUND = 2
+EXIT_DATABASE_ERROR = 3
+
 def create_connection(db_config):
     """
     Create a database connection using the provided configuration.
@@ -16,7 +22,11 @@ def create_connection(db_config):
     :param db_config: A dictionary containing the DB connection parameters
     :return: A new database connection object
     """
-    return psycopg2.connect(**db_config)
+    try:
+        return psycopg2.connect(**db_config)
+    except psycopg2.Error as e:
+        print(f"Failed to connect to the database: {e}")
+        return EXIT_DATABASE_ERROR
 
 def add_event(conn, title, event_type_id, start_time, end_time, description, room_id):
     """
@@ -29,13 +39,19 @@ def add_event(conn, title, event_type_id, start_time, end_time, description, roo
     :param end_time: The end time of the event
     :param description: The description of the event
     :param room_id: The ID of the room where the event takes place
+    :return: The exit code
     """
-    with conn.cursor() as cursor:
-        cursor.execute("""
-            INSERT INTO event (title, event_type_id, start_time, end_time, description, room_id)
-            VALUES (%s, %s, %s, %s, %s, %s)
-        """, (title, event_type_id, start_time, end_time, description, room_id))
-        conn.commit()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                INSERT INTO event (title, event_type_id, start_time, end_time, description, room_id)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """, (title, event_type_id, start_time, end_time, description, room_id))
+            conn.commit()
+        return EXIT_SUCCESS
+    except psycopg2.Error as e:
+        print(f"Failed to add event to the database: {e}")
+        return EXIT_DATABASE_ERROR
 
 def bulk_add_events(conn, filepath):
     """
@@ -43,21 +59,34 @@ def bulk_add_events(conn, filepath):
 
     :param conn: The database connection object
     :param filepath: The path to the file containing the events data
+    :return: The exit code
     """
-    with open(filepath, 'r') as file:
-        for line in file:
-            title, event_type_id, start_time, end_time, description, room_id = line.strip().split(',')
-            add_event(conn, title, event_type_id, start_time, end_time, description, room_id)
+    try:
+        with open(filepath, 'r') as file:
+            for line in file:
+                title, event_type_id, start_time, end_time, description, room_id = line.strip().split(',')
+                exit_code = add_event(conn, title, event_type_id, start_time, end_time, description, room_id)
+                if exit_code != EXIT_SUCCESS:
+                    return exit_code
+        return EXIT_SUCCESS
+    except FileNotFoundError:
+        print(f"File not found: {filepath}")
+        return EXIT_FILE_NOT_FOUND
 
 def main():
     """
     The main function of the program.
     """
     conn = create_connection(DB_CONFIG)
+    exit_code = EXIT_SUCCESS
     
     if '-b' in sys.argv:
-        filepath = sys.argv[sys.argv.index('-b') + 1]
-        bulk_add_events(conn, filepath)
+        if len(sys.argv) < 3:
+            print("Invalid arguments. Usage: python event_importer.py -b <filepath>")
+            exit_code = EXIT_INVALID_ARGUMENTS
+        else:
+            filepath = sys.argv[sys.argv.index('-b') + 1]
+            exit_code = bulk_add_events(conn, filepath)
     else:
         # Interactive mode for adding a single event
         title = input("Enter event title: ")
@@ -66,9 +95,11 @@ def main():
         end_time = input("Enter end time (YYYY-MM-DD HH:MM:SS): ")
         description = input("Enter event description: ")
         room_id = input("Enter room ID: ")
-        add_event(conn, title, event_type_id, start_time, end_time, description, room_id)
+        exit_code = add_event(conn, title, event_type_id, start_time, end_time, description, room_id)
     
     conn.close()
+    return exit_code
 
 if __name__ == "__main__":
-    main()
+    exit_code = main()
+    sys.exit(exit_code)

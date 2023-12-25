9cd4db8de03a3dde79c23dc4f114311c8049eb4b
SiemensHalske <hendrksiemens1@gmail.com>
2023-12-25 16:02:23 +0100

    Update CPU and RAM gauges in refresh_data.js

--- static/admin/refresh_data.js
+++ static/admin/refresh_data.js
@@ -1,46 +1,58 @@
-$(document).ready(function() {
-    var cpuGauge = new JustGage({
-        id: "cpu-usage",
-        value: 0,
-        min: 0,
-        max: 100,
-        title: "CPU Usage"
-    });
+$(document).ready(function () {
+  var cpuGauge = new JustGage({
+    id: "cpu-usage",
+    value: 0,
+    min: 0,
+    max: 100,
+    title: "CPU Usage",
+    width: 200,
+    height: 200,
+  });
 
-    var ramGauge = new JustGage({
-        id: "ram-usage",
-        value: 0,
-        min: 0,
-        max: 100,
-        title: "RAM Usage"
-    });
+  var ramGauge = new JustGage({
+    id: "ram-usage",
+    value: 0,
+    min: 0,
+    max: 100,
+    title: "RAM Usage",
+    width: 200,
+    height: 200,
+    textRenderer: function (value) {
+      var absoluteValue = (value * 4096) / 100; // totalRam should be the total RAM in MB
+      return value + "% (" + absoluteValue.toFixed(2) + " MB)";
+    },
+  });
 
-    var cpuTempGauge = new JustGage({
-        id: "cpu-temp",
-        value: 0,
-        min: 0,
-        max: 100,
-        title: "CPU Temperature"
-    });
+  var cpuTempGauge = new JustGage({
+    id: "cpu-temp",
+    value: 0,
+    min: 0,
+    max: 100,
+    title: "CPU Temperature",
+    width: 200,
+    height: 200,
+  });
 
-    var ramTempGauge = new JustGage({
-        id: "ram-temp",
-        value: 0,
-        min: 0,
-        max: 100,
-        title: "RAM Temperature"
-    });
+  var ramTempGauge = new JustGage({
+    id: "ram-temp",
+    value: 0,
+    min: 0,
+    max: 100,
+    title: "RAM Temperature",
+    width: 200,
+    height: 200,
+  });
 
-    setInterval(function() {
-        $.ajax({
-            url: '/system_info',
-            type: 'GET',
-            success: function(data) {
-                cpuGauge.refresh(data.cpu_usage);
-                ramGauge.refresh(data.ram_usage);
-                cpuTempGauge.refresh(data.cpu_temp);
-                ramTempGauge.refresh(data.ram_temp);
-            }
-        });
-    }, 1000);
+  setInterval(function () {
+    $.ajax({
+      url: "/system_info",
+      type: "GET",
+      success: function (data) {
+        cpuGauge.refresh(data.cpu_usage);
+        ramGauge.refresh(data.ram_usage_bytes / 1024 / 1024);
+        cpuTempGauge.refresh(data.cpu_temp);
+        ramTempGauge.refresh(data.ram_temp);
+      },
+    });
+  }, 1000);
 });

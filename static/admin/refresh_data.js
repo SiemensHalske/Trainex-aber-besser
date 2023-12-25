38d2c811c8dbac35be5c307a5968dc986c29fc19
SiemensHalske <hendrksiemens1@gmail.com>
2023-12-25 22:12:19 +0100

    Update dashboard layout and system info display

--- static/admin/refresh_data.js
+++ static/admin/refresh_data.js
@@ -1,60 +1,43 @@
-$(document).ready(function () {
-  var cpuGauge = new JustGage({
-    id: "cpu-usage",
+var cpuUsageGauge = new JustGage({
+    id: "cpuUsageGauge",
     value: 0,
     min: 0,
     max: 100,
-    title: "CPU Usage",
-    width: 200,
-    height: 200,
-  });
-
-  var ramGauge = new JustGage({
-    id: "ram-usage",
-    value: 0,
-    min: 0,
-    max: 4096,
-    title: "RAM Usage",
-    width: 200,
-    height: 200,
-    textRenderer: function (value) {  // value is the value in MB
-      var maxRam = 4096;  // # in MB
-      var percentage_used = value / maxRam * 100;
-      return percentage_used.toFixed(2) + "%\n" + " (" + value + "MB)";
-    },
-  });
-
-  var cpuTempGauge = new JustGage({
-    id: "cpu-temp",
-    value: 0,
-    min: 0,
-    max: 100,
-    title: "CPU Temperature",
-    width: 200,
-    height: 200,
-  });
-
-  var ramTempGauge = new JustGage({
-    id: "ram-temp",
-    value: 0,
-    min: 0,
-    max: 100,
-    title: "RAM Temperature",
-    width: 200,
-    height: 200,
-  });
-
-  setInterval(function () {
-    $.ajax({
-      url: "/system_info",
-      type: "GET",
-      success: function (data) {
-        cpuGauge.refresh(data.cpu_usage);
-        //  # totalRam should be the total RAM in MB
-        ramGauge.refresh(data.ram_usage_bytes / 1024 / 1024);
-        cpuTempGauge.refresh(data.cpu_temp);
-        ramTempGauge.refresh(data.ram_temp);
-      },
-    });
-  }, 1000);
+    title: "CPU Usage"
 });
+
+var cpuTempGauge = new JustGage({
+    id: "cpuTempGauge",
+    value: 0,
+    min: 0,
+    max: 100,
+    title: "CPU Temperature"
+});
+
+var ramUsageGauge = new JustGage({
+    id: "ramUsageGauge",
+    value: 0,
+    min: 0,
+    max: 100,
+    title: "RAM Usage %"
+});
+
+var ramUsageBar = document.getElementById('ramUsageBar');
+
+function updateSystemInfo() {
+    $.ajax({
+        url: '/system_info',
+        method: 'GET',
+        success: function(data) {
+            cpuUsageGauge.refresh(data.cpu_usage);
+            cpuTempGauge.refresh(data.cpu_temperature);
+            ramUsageGauge.refresh(data.ram_usage_percent);
+
+            var ramUsageMB = data.ram_usage_bytes / 1048576; // Convert bytes to MB
+            ramUsageBar.style.width = data.ram_usage_percent + '%';
+            ramUsageBar.innerText = ramUsageMB.toFixed(2) + ' MB'; // Display RAM usage in MB
+        }
+    });
+}
+
+setInterval(updateSystemInfo, 1000);

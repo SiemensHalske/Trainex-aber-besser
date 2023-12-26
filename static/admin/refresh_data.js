fb6a1c0fad2d208f3b51949c8f85c3e6d5f36769
SiemensHalske <hendrksiemens1@gmail.com>
2023-12-26 11:11:00 +0100

    Refactor dashboard gauge update logic

--- static/admin/refresh_data.js
+++ static/admin/refresh_data.js
@@ -40,22 +40,41 @@ $(document).ready(function () {
     decimals: 2, // Set the number of decimal places
   });
 
-  // Function to update gauges
   function updateGauges() {
-    $.getJSON("/system_info", function (data) {
-      console.log(data);
-      cpuGauge.refresh(data.cpu_usage);
-      memGauge.refresh(data.ram_usage_bytes / 1048576); // Convert to MB
-      cpuTempGauge.refresh(data.cpu_temperature);
-      ramTempGauge.refresh(data.ram_temperature);
+    // Check if the dashboard tab is active before updating gauges
+    if ($('a[href="#dashboard"]').hasClass("active")) {
+      $.getJSON("/system_info", function (data) {
+        console.log(data);
+        cpuGauge.refresh(data.cpu_usage);
+        memGauge.refresh(data.ram_usage_bytes / 1048576); // Convert to MB
+        cpuTempGauge.refresh(data.cpu_temperature);
+        ramTempGauge.refresh(data.ram_temperature);
 
-      // Update absolute memory usage text
-      $("#memory_usage_absolute").text(
-        (data.ram_usage_bytes / 1048576).toFixed(2) + " MB"
-      );
-    });
+        // Update absolute memory usage text
+        $("#memory_usage_absolute").text(
+          (data.ram_usage_bytes / 1048576).toFixed(2) + " MB"
+        );
+      });
+    }
   }
 
-  // Update gauges every second
+  // Initial call to update gauges
+  updateGauges();
+
+  // Set interval to update gauges every second if dashboard is active
   setInterval(updateGauges, 1000);
+
+  // Add click event to update gauges when the dashboard tab is clicked
+  $('a[href="#dashboard"]').click(function () {
+    // Add 'active' class to dashboard link
+    $(this).addClass("active");
+    // Immediately update gauges when dashboard is clicked
+    updateGauges();
+  });
+
+  // Add click event to remove 'active' class when other tabs are clicked
+  $('a[href]:not([href="#dashboard"])').click(function () {
+    // Remove 'active' class from dashboard link
+    $('a[href="#dashboard"]').removeClass("active");
+  });
 });
